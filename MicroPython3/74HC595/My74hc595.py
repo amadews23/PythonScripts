03f93667c4dc1eff6c84e3a5ddbc74dd37d2abed
B. Vich <tolo@tovilo.es>
2024-04-19 20:54:41 +0200

    Update My74hc595.py

--- MicroPython3/74HC595/My74hc595.py
+++ MicroPython3/74HC595/My74hc595.py
@@ -16,18 +16,54 @@ dataPin=Pin(dataPin, Pin.OUT)
 latchPin=Pin(latchPin, Pin.OUT)
 clockPin=Pin(clockPin, Pin.OUT)
 
-def send(data_bin):
+def prepare_to_get():
   #put latch down to start data sending
   clockPin.value(0)
   latchPin.value(0)
   clockPin.value(1)
-  
-  for i in range ( len(data_bin)):
-    clockPin.value(0)
-    dataPin.value(int(data_bin[i]))
-    clockPin.value(1)
-    
+
+def execute_to_send():
   #put latch up to send data to the outputs   
   clockPin.value(0)
   latchPin.value(1)
   clockPin.value(1)
+
+'''
+example to use 
+My74hc595.send_num('10101010')
+'''    
+def send(data):
+
+  prepare_to_get()
+  
+  for i in range ( len(data)):
+    clockPin.value(0)
+    dataPin.value(int(data[i]))
+    clockPin.value(1)
+  
+  execute_to_send() 
+
+'''
+#example to use, count 0 t0 255
+from time import sleep_ms
+for a in range (256):
+  My74hc595.send_num(a)
+  sleep_ms(100)
+''' 
+
+def send_num(data):
+
+  data = str(bin(data)[2:])
+  num_dif = 8 - len(data)
+  print((num_dif*"0")+data)
+  new_data = (num_dif*"0")+data
+
+  prepare_to_get()
+  
+  for i in range (len(new_data)):
+    clockPin.value(0)
+    dataPin.value(int(new_data[i]))
+    clockPin.value(1)
+  
+  execute_to_send()
+  
